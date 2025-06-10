b8558ab55c07fca8c735d73f8c04568b9bb52312
ellen-rose-james <ellenrj03@gmail.com>
2025-06-11 03:11:16 +0530

    feat: add hover effects and add typed text animation

--- assets/global.js
+++ assets/global.js
@@ -5,3 +5,16 @@ AOS.init({
     easing: 'ease-in-out',
     offset: 20,
 });
+
+const text = "Hi! I'm Ellen";
+let index = 0;
+function type() {
+  if (index < text.length) {
+    document.getElementById("typed-text").textContent += text.charAt(index);
+    index++;
+    setTimeout(type, 100);
+  }
+}
+window.onload = type;
+
+
